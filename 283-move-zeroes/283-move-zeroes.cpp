21278bc943abb84282cb4592d10ac2cbaa156d30
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2022-09-19 22:39:53 +0530

    Time: 48 ms (35.03%), Space: 19.1 MB (83.32%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int>ans;
-        for(int i = 0; i<nums.size(); i++){
-            if(nums[i] != 0){
-                ans.push_back(nums[i]);
-            }
+
+        int front = 0 , end = nums.size()-1 , n = nums.size();
+        int countZero = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] != 0)
+                nums[front++] = nums[i];               
+            else if(nums[i] == 0)
+                countZero++;
         }
-        for(int i = 0; i<ans.size(); i++){
-            nums[i] = ans[i];
-        }
-        for(int i = ans.size() ; i<nums.size(); i++){
+//         if(countZero == 0)
+//             return;
+// //         handling {0,1} exception here
+//         if(countZero == 1)
+            // nums[nums.size() - 1] = 0;
+        for(int i = n-1; i>= (n - countZero ); i--)
             nums[i] = 0;
-        }
+        
     }
 };
