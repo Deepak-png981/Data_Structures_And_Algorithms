24a09ef740f59d512fef384043fbb83a04104a65
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2022-09-25 21:03:53 +0530

    Time: 3 ms (63.91%), Space: 5.8 MB (78.60%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     int fib(int n) {
-        int first = 0 , second = 1 , fib;
-        for(int i = 0; i<n-1; i++){
-            fib = first + second;
-            first = second;
-            second = fib;
+        // int first = 0 , second = 1 , fib;
+        // for(int i = 1; i<n; i++){
+        //     fib = first + second;
+        //     first = second;
+        //     second = fib;
+        // }
+        // return fib;
+        
+//         using two variables only
+        int a = 0 , b = 1;
+        if(n == 0)
+            return 0;
+        for(int i = 1; i<n; i++){
+            b += a;
+            a = b - a;
         }
-        return fib;
+        return b;
     }
 };
