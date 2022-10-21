64fe2ecbe7f255201b51d87c85d58a1f7614c138
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2022-10-21 15:32:18 +0530

    Time: 25 ms (62.64%), Space: 13.2 MB (54.87%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -19,7 +19,7 @@ public:
   //and setting values of matrix accordingly.
   for (int i = rows - 1; i >= 0; i--) {
     for (int j = cols - 1; j >= 1; j--) {
-      if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+      if (matrix[i][0] == 0 or matrix[0][j] == 0) {
         matrix[i][j] = 0;
       }
     }
