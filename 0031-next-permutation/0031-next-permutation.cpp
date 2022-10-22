84c4f2c389661a9293ab02d5589a52ef9c212bda
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2022-10-22 08:22:20 +0530

    Time: 15 ms (10.14%), Space: 12.1 MB (74.87%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,9 +1,34 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        next_permutation(nums.begin() , nums.end());
-        // next_permutation(nums , nums + n );
+//    Using inbuilt function
+        // int n = nums.size();
+        // next_permutation(nums.begin() , nums.end());
         
+//         using logic
+        int n = nums.size();
+        int idx = -1;
+        //traversing from the back of the array
+        for(int i = n-1; i>0; i--){
+            //comparing .. finding out the index for which the next element is smaller than the current element
+            if(nums[i] > nums[i-1]){
+                idx = i; //idx mein i store ker liye
+                break;
+            }
+        }
+        //decreasnig hoga to wo handle kiya hai yaha
+        if(idx == -1){
+            reverse(nums.begin() , nums.end());
+        }
+        else{
+        int prev = idx;
+            for(int i = idx+1; i<n; i++){
+                if(nums[idx-1] < nums[i] and nums[i] <= nums[prev])
+                    prev = i;
+            }
+            swap(nums[idx-1] , nums[prev]);
+            reverse(nums.begin() + idx , nums.end());
+        }
+        return;
     }
 };
