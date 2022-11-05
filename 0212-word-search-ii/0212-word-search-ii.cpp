28f7e2c6b642279812fec2c2b3df06541281974f
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2022-11-05 13:42:17 +0530

    Time: 1168 ms (54.88%), Space: 15.4 MB (35.69%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -1,148 +1,106 @@
 class Solution
-{
-
-    public:
-     // declare a structure for TrieNode
-
+{ 
     struct TrieNode
-    {
-        bool is_end;
-
-        string word;
-
-        TrieNode* child[26];
-
-        TrieNode()
         {
-            is_end = false;
-
-            word = "";
-
-            for(int i = 0; i < 26; i++)
+            bool isEnd;
+            string word;
+            TrieNode *child[26];
+            TrieNode()
             {
-                child[i] = NULL;
+                isEnd = false;
+                word = "";
+                for (int i = 0; i < 26; i++)
+                    child[i] = NULL;
             }
-        }
+        };
+    private:
+    //global result vector to store all strings which are present in board
+    vector<string> res;
+   	//direction array to move
+    vector<int> dx = { -1,
+        0,
+        1,
+        0
+    };
+    vector<int> dy = { 0,
+        1,
+        0,
+        -1
     };
 
-    // declare root of the Trie
-
-    TrieNode* root = new TrieNode();
-
-    // function for inserting word into Trie
-
-    void insert(string& str)
-    {
-        int n = str.size();
-
-        TrieNode* curr = root;
-
-        for(int i = 0; i < n; i++)
-        {
-            int idx = str[i] - 'a';
-
-            // if no subtree is present then insert the new node
-
-            if(curr -> child[idx] == NULL)
-            {
-                curr -> child[idx] = new TrieNode();
-            }
-
-            curr = curr -> child[idx];
-        }
-
-        // update is_end and word
-
-        curr -> is_end = true;
-
-        curr -> word = str;
-    }
-
-    // dfs function
-
-    vector<string> res;
-
-    // direction co-ordinates of all four directions
-
-    vector<int> dx = {-1, 0, 1, 0};
-
-    vector<int> dy = {0, 1, 0, -1};
-
-    void dfs(vector<vector<char>>& grid, int i, int j, int n, int m, TrieNode* curr)
-    {
-        // base case
-         
-        if(i < 0 || i >= n || j < 0 || j >= m || grid[i][j] == '#')
-        {
-            return;
-        }
-
-        int idx = grid[i][j] - 'a';
-
-        // if there is no word starting with curr character then return
-
-        if(curr -> child[idx] == NULL)
-        {
-            return;
-        }
-
-        // move curr
-
-        curr = curr -> child[idx];
-
-        // if we found a word
-
-        if(curr -> is_end)
-        {
-            res.push_back(curr -> word);
-
-            // mark is_end with false to avoid duplicate result
-
-            curr -> is_end = false;
-        }
-
-        // store the val of curr cell
-
-        char val = grid[i][j];
-
-        // mark the curr cell visited
-
-        grid[i][j] = '#';
-
-        // explore all the four directions
-
-        for(int k = 0; k < 4; k++)
-        {
-            int new_i = i + dx[k];
-
-            int new_j = j + dy[k];
-
-            // call dfs
-
-            dfs(grid, new_i, new_j, n, m, curr);
-        }
-
-        // backtrack
-
-        grid[i][j] = val;
-    }
+    public:
+       
+   	//declaring the global root for the trienode
+    TrieNode *root = new TrieNode();
     
-    vector<string> findWords(vector<vector<char>>& grid, vector<string>& words) {
-        
-        int n = grid.size();
-        
-        int m = grid[0].size();
-
-        // insert all the words into trie
-
-        for(auto word : words)
+   	//function to insert in the TRIE;
+    void insert(string & word)
+    {
+       	//size of the string
+        int n = word.length();
+       	//creating a new node
+        TrieNode *curr = root;
+       	//traversing through the word
+        for (int i = 0; i < n; i++)
         {
-            insert(word);
+            int indx = word[i] - 'a';
+           	//if no child or similar subtrie is present then insert it
+            if (curr->child[indx] == NULL)
+            {
+                curr->child[indx] = new TrieNode();
+            }
+            curr = curr->child[indx];
         }
+        curr->isEnd = true;
+        curr->word = word;
+    }
 
-        // start dfs from all the cell and find possible result
+  
+   	// dfs
+    void dfs(vector<vector < char>> &grid, int i, int j, int n, int m, TrieNode *curr)
+    {
+       	//base case
+        if (i < 0 or i >= n or j < 0 or j >= m or grid[i][j] == '#')
+            return;
 
-        for(int i = 0; i < n; i++)
+        int inx = grid[i][j] - 'a';
+       	// if there is no word starting with current character then return
+        if (curr->child[inx] == NULL)
+        {
+            return;
+        }
+       	//if exist then move curr
+        curr = curr->child[inx];
+       	//if a word is found
+        if (curr->isEnd)
+        {
+            res.push_back(curr->word);
+           	//now mark its isEnd as false
+            curr->isEnd = false;
+        }
+       	//storing the character to use it further after coming back from the recursion
+        char tmpval = grid[i][j];
+        grid[i][j] = '#';
+        for (int k = 0; k < 4; k++)
+        {
+            int newi = i + dx[k];
+            int newj = j + dy[k];
+           	//now calling dfs for different different directions
+            dfs(grid, newi, newj, n, m, curr);
+        }
+       	//after coming back from recursion we are backtracking
+        grid[i][j] = tmpval;
+    }
+    vector<string> findWords(vector<vector < char>> &grid, vector< string > &words)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+       	//inserting words into the trie
+        for (auto it: words)
+        {
+            insert(it);
+        }
+         for(int i = 0; i < n; i++)
         {
             for(int j = 0; j < m; j++)
             {
