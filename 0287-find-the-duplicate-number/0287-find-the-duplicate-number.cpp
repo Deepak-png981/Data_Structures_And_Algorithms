326725be8a9b685cbdf6e5afb6384fdf5c64ba8a
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2022-11-05 16:13:04 +0530

    Time: 218 ms (72.56%), Space: 61.3 MB (62.16%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -4,7 +4,7 @@ public:
         //size of nums
         int n = nums.size();
         for(int i = 0 ; i <n; i++){
-            //finding the index so that I can multiply by -1 further
+            //(nums[i]finding the index so that I can multiply by -1 further
             int index = abs(nums[i]) - 1;
             //multiplying the nums[index] with -1 , if it is occuring two times
             //then it will automatically convert into positive
