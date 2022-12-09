00203b2adcee6d6b9df527a25dda9ef8430e2aa9
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2022-12-09 19:57:15 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (45.53%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -3,20 +3,18 @@ public:
     vector<vector<int>> helper(vector<int>&nums , int i){
         if(i == nums.size())
             return {{}};
-        vector<vector<int>> partialAns = helper(nums , i + 1);
+        vector<vector<int>> partialans = helper(nums , i+1);
         vector<vector<int>> ans;
-        for(vector<int> x : partialAns){
+        for(vector<int>x : partialans){
             ans.push_back(x);
         }
-        for(vector<int>x : partialAns){
+        for(vector<int>x : partialans){
             x.push_back(nums[i]);
             ans.push_back(x);
         }
         return ans;
-        
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         return helper(nums , 0);
-        
     }
 };
