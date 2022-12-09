3f2d8b94a974d9d1b762fc66b0f104dcc2ffe34f
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2022-12-09 13:57:05 +0530

    Added solution - LeetHub

--- Black and White - GFG/black-and-white.cpp
+++ Black and White - GFG/black-and-white.cpp
@@ -23,21 +23,21 @@ int main() {
 //white Knight on this chessboard such that they cannot attack each other.
 long long numOfWays(int n, int m)
 {
-    int dx[] = {2 , 2, 1,1 , -1, -1 , -2 , -2};
-    int dy[] = {-1 , 1 , -2 , 2 , -2 , 2, -1, 1};
-    long long mod = 1000000007;
-    long long total = ( ((n*m)%mod) * ((n*m - 1)%mod) )%mod;
-    long long attack = 0;
-    for(int i= 0; i<n; i++){
-        for(int j = 0; j<m; j++){
-            for(int k = 0; k<8; k++){
-                int newx = i+dx[k];
-                int newy = j+dy[k];
-                if(newx >= 0 and newx < n and newy >= 0 and newy <m){
-                    attack++;
-                }
-            }
-        }
-    }
-    return (total%mod - attack%mod)%mod;
+   long long mod = 1000000007;
+   int dx[] = {2 ,2 , 1, 1, -1 , -1, -2, -2};
+   int dy[] = {-1 , 1, -2, 2, -2 , 2, -1 ,1};
+   long long total = ( (m*n)%mod * (m*n - 1)%mod )%mod;
+   long long attack = 0;
+   for(int i = 0; i<n; i++){
+       for(int j = 0; j<m; j++){
+           for(int k = 0; k<8; k++){
+               int newx = i + dx[k];
+               int newy = j + dy[k];
+               if(newx >= 0 and newx < n and newy >= 0 and newy <m)
+                attack++;
+           }
+       }
+   }
+   return (total%mod - attack%mod)%mod;
+   
 }
