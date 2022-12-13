c3b3445a0976820c7b2e79b8274e14d3094292fa
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2022-12-14 00:53:13 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.cpp
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -4,45 +4,42 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-    private:
-    void bfs(int row , int col ,vector<vector<int>>&vis,vector<vector<char>>& grid ){
-        int n = grid.size();
-        int m = grid[0].size();
-        vis[row][col] = 1;
-        queue<pair<int ,int>>q;
-        q.push({row , col});
+  private:
+    void dfs(int i , int j , 
+    vector<vector<int>>& vis, int n , int m , vector<vector<char>>& grid){
+        vis[i][j] = 1;
+        queue<pair<int , int>> q;
+        q.push({i , j});
         while(!q.empty()){
-            pair<int, int>frontN = q.front();
+            int frontRow = q.front().first;
+            int frontCol = q.front().second;
             q.pop();
-            int rowQ = frontN.first;
-            int colQ = frontN.second;
-            //now we need to traverse in the neighbours and mark it if its a land
-            for(int nrow = -1 ; nrow <=1; nrow++){
-                for(int ncol = -1; ncol <= 1; ncol++){
-                    int newrow = rowQ + nrow;
-                    int newcol = colQ + ncol;
-                    if(newrow >= 0 and newrow < n and newcol >=0 and newcol<m 
-                    and grid[newrow][newcol] == '1' and !vis[newrow][newcol]){
-                        vis[newrow][newcol] = 1;
-                        q.push({newrow , newcol});
+            //ye do forloop lagane pdht hai aatho directions mein jane ke liye
+            for(int i = -1 ; i<= 1 ; i++){
+                for(int j = -1; j<=1; j++){
+                    int newx = frontRow + i;
+                    int newy = frontCol + j;
+                    if(newx >= 0 and newx < n and newy >= 0 and newy < m
+                    and vis[newx][newy] == 0 and grid[newx][newy] == '1'){
+                        vis[newx][newy] = 1;
+                        q.push({newx , newy});
                     }
                 }
             }
         }
+        
     }
   public:
     // Function to find the number of islands.
     int numIslands(vector<vector<char>>& grid) {
-        // Code here
-        int n = grid.size();
-        int m = grid[0].size();
         int count = 0;
-        vector<vector<int>>vis(n , vector<int>(m,0));
-        for(int i = 0 ; i<n; i++){
-            for(int j = 0 ; j <m; j++){
-                if(!vis[i][j] and grid[i][j] == '1'){
+        int n = grid.size() , m = grid[0].size();
+        vector<vector<int>>vis (n , vector<int>(m , 0));
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(vis[i][j] == 0 and grid[i][j] == '1') {
                     count++;
-                    bfs(i , j , vis , grid);
+                    dfs(i , j, vis , n , m,grid);
                 }
             }
         }
