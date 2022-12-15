a9e77608ec2c276748ddd6b23af3ccf82458eb18
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2022-12-15 13:05:18 +0530

    Added solution - LeetHub

--- Replace O's with X's - GFG/replace-os-with-xs.cpp
+++ Replace O's with X's - GFG/replace-os-with-xs.cpp
@@ -9,60 +9,45 @@ using namespace std;
 
 class Solution{
 private:
-void dfs(int row , int col , vector<vector<int>>&vis , vector<vector<char>>mat,
-            int delrow[] , int delcol[]){
-    int n = mat.size();
-    int m = mat[0].size();
-    vis[row][col] = 1;
-    for(int i = 0 ; i<4; i++){
-        int nrow = row + delrow[i];
-        int ncol = col + delcol[i];
-        if(nrow >= 0 and nrow < n and ncol >= 0 and ncol < m and !vis[nrow][ncol] 
-            and mat[nrow][ncol] == 'O')
-        {
-            dfs(nrow , ncol , vis , mat , delrow , delcol);
+    void dfs(int row , int col , vector<vector<int>>&vis , vector<vector<char>> mat){
+        vis[row][col] = 1;
+        int n = mat.size();
+        int m = mat[0].size();
+        int dx[] = { -1 , 0 , +1 , 0};
+        int dy[] = { 0 , +1 , 0 , -1};
+        for(int i = 0; i<4; i++){
+            int nrow = row + dx[i];
+            int ncol = col + dy[i];
+            if(nrow >= 0 and nrow < n and ncol >= 0 and ncol < m and 
+                vis[nrow][ncol] == 0 and mat[nrow][ncol] == 'O')
+                dfs(nrow , ncol , vis , mat);
         }
-        
     }
-}
 public:
     vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
     {
-        /* approach ye hai ki jo bhi 'O' first ya last row mein aaye
-           ya first ya last column mein aaye ushper dfs laga do takie
-           hum sare 'O's ko waise hi visite ker ske or ek visited name
-           ke vector mein un corner per ane wale sabhi 'O's ki locations 
-           ko store kr lenge taake jab final mein hum mat matrix ko trace
-           kare to un 'o's ko kuch na kare bakiyo ko change ker de.......
-           wuuuuuuuuuuuuuuuuu huuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu
-         */
-        vector<vector<int>> vis( n , vector<int> (m , 0));
-        int delrow[] = {-1 , 0 , +1, 0};
-        int delcol[] = {0 , +1 , 0 , -1};
-         //for first row and last row
-         for(int j = 0; j<m; j++){
-             //first row
-             if(mat[0][j] == 'O' and vis[0][j] == 0){
-                dfs(0 , j , vis , mat , delrow , delcol);
-             }
-             //last row
-             if(mat[n-1][j] == 'O' and vis[n-1][j] == 0)
-                dfs(n-1 , j , vis , mat , delrow , delcol);
-         }
-         //for first and last column
-        for(int i = 0; i<n; i++){
-             //for first col
-             if(vis[i][0] == 0 and mat[i][0] == 'O')
-                dfs(i , 0 , vis , mat , delrow , delcol);
-             if(vis[i][m-1] == 0 and mat[i][m-1] == 'O')
-                dfs(i , m-1 , vis ,mat, delrow , delcol);
+        vector<vector<int>>vis(n , vector<int>(m , 0));
+        //first row and last row
+        for(int i = 0; i < m; i++){
+            if(vis[0][i] == 0 and mat[0][i] == 'O'){
+                dfs(0 , i , vis , mat);
+            }
+            if(vis[n-1][i] == 0 and mat[n-1][i] == 'O')
+                dfs( n-1 , i , vis , mat);
         }
-        //ab replace ker rhe hai bache hue O's ko X se jo boundary se 
-        //connected nhi hai
+        //for first and last col
         for(int i = 0; i<n; i++){
-            for(int j = 0 ; j<m ; j++){
-                if(vis[i][j] == 0 and mat[i][j] == 'O')
+            if(vis[i][0] == 0 and mat[i][0] == 'O')
+                dfs(i , 0 , vis , mat);
+            if(vis[i][m-1] == 0 and mat[i][m-1] == 'O')
+                dfs(i , m-1 , vis , mat);
+        }
+        //sabhi ko X kerdo except unke jinka visited already 1 hai
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(vis[i][j] == 0){
                     mat[i][j] = 'X';
+                }
             }
         }
         return mat;
