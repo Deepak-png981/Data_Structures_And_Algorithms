2e2c0dcc5a4ec2c6f8fdd90d0b6c8b0395107859
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2022-12-24 12:16:34 +0530

    Time: 3 ms (55.84%), Space: 6.1 MB (71.49%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -1,23 +1,23 @@
 class Solution {
-// https://leetcode.com/problems/domino-and-tromino-tiling/discuss/2944400/C%2B%2B-Simple-idea-and-code-(0ms-runtime).-Explain-by-image-and-comment.
 public:
     int numTilings(int n) {
-         int mod = 1000000007;
         if(n == 1)
             return 1;
         if(n == 2)
             return 2;
         if(n == 3)
             return 5;
-        long a = 1 , b = 2 , c = 5, res  = 0;
-        //logic is An = An-3 + An-1 * 2
+        int mod = 1000000007;
+        long a = 1 , b = 2 , c = 5 , res = 0;
         while(n--){
-            if(n < 3)
+            if(n < 3){
                 break;
-            res = (2*c + a)%mod;
+            }
+            res = (a + 2*c)%mod;
             a = b;
             b = c;
             c = res;
+            
         }
         return res;
     }
