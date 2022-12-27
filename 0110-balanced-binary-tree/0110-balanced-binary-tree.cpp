4a2b8ff8c78f6a28d278873839fe46b1a98a60f0
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2022-12-27 20:22:56 +0530

    Time: 8 ms (96.46%), Space: 22.1 MB (6.05%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,34 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
-private:
-    int height(TreeNode* root){
-        if(!root)
-            return 0;
-        int l = height(root->left);
-        int r = height(root->right);
-        int ans = max(l , r) + 1;
-        return ans;
+private: 
+    //in pair bool is for balanced tree checking and int is for height
+    pair<bool , int> balheight(TreeNode * root){
+        if(!root){
+            pair<bool , int> p;
+            p.first = true;
+            p.second = 0;
+            return p;
+        }
+        pair<bool , int> left = balheight(root->left);
+        pair<bool , int> right = balheight(root->right);
+        
+        bool op1 = left.first;
+        bool op2 = right.first;
+        bool op3 = abs(left.second - right.second) <= 1;
+        
+        pair<bool , int>p;
+        if(op1 and op2 and op3)
+            p.first = true;
+        else
+            p.first = false;
+        p.second = max(left.second , right.second) + 1;
+        return p;
     }
 public:
     bool isBalanced(TreeNode* root) {
-        if(root == NULL){
-            return true;
-        }
-        bool left = isBalanced(root->left);
-        bool right = isBalanced(root->right);
-        bool diff = abs( height(root->right) - height(root->left) ) <= 1;
-        if(left and right and diff)
-            return true;
-        return false;
+        pair<bool , int>p = balheight(root);
+        return p.first;
     }
 };
