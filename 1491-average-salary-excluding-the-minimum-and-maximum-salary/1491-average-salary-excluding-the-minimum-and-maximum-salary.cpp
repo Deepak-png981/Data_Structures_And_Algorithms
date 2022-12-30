d9d2301d7e993070a89d9e8dea89e818d86b00f4
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2022-12-30 21:58:12 +0530

    Time: 4 ms (36.77%), Space: 7.1 MB (47.23%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -1,14 +1,30 @@
 class Solution {
 public:
     double average(vector<int>& arr) {
+        //nlogn
+        // if(arr.size() <= 2)
+        //     return 0;
+        // sort(arr.begin() , arr.end());
+        // int n = arr.size();
+        // double sum = 0;
+        // for(int i = 1; i<n-1; i++){
+        //     sum += arr[i];
+        // }
+        // return (sum/(n-2));
+        
+        
+        //o(n)
         if(arr.size() <= 2)
             return 0;
-        sort(arr.begin() , arr.end());
+        int mx = INT_MIN , mn = INT_MAX;
         int n = arr.size();
-        double sum = 0;
-        for(int i = 1; i<n-1; i++){
+        double sum = 0.00000;
+        for(int i = 0; i<n; i++){
+            mx = max(mx , arr[i]);
+            mn = min(mn , arr[i]);
             sum += arr[i];
         }
-        return (sum/(n-2));
+        return (sum - (mx + mn))/(n-2);
+        
     }
 };
