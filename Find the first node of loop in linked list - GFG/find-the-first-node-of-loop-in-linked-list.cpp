974034b91283fe643bdea86f5b73d8835f88a13e
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-01-18 20:08:03 +0530

    Added solution - LeetHub

--- Find the first node of loop in linked list - GFG/find-the-first-node-of-loop-in-linked-list.cpp
+++ Find the first node of loop in linked list - GFG/find-the-first-node-of-loop-in-linked-list.cpp
@@ -46,22 +46,33 @@ class Solution
      //Function to find first node if the linked list has a loop.
     int findFirstNode(Node* head)
     {
-         Node* slow = head;
+        Node* slow = head;
         Node* fast = head;
-        while(slow != NULL and fast != NULL and fast->next != NULL){
-            slow = slow->next;
+        while(slow != NULL and fast!= NULL and fast->next != NULL){
             fast = fast->next->next;
-            if(slow == fast)
-                break;
-        }
-        if(slow != fast)    return -1;
-        slow = head;
-        while(slow != fast)
-        {
             slow = slow->next;
-            fast = fast->next;
+            if(slow == fast){
+                break;
+            }
         }
-        return slow->data;
+        if(slow != fast)
+            return -1;
+        if(slow == head){
+            fast = head;
+            while( fast->next != slow){
+                fast = fast->next;
+            }
+            return slow->data;
+        }
+        else if( slow == fast){
+            slow = head;
+            while(slow -> next != fast -> next)
+            {
+                slow = slow -> next;
+                fast = fast->next;
+            }
+            return fast->next->data;
+        }
     }
 };
 
