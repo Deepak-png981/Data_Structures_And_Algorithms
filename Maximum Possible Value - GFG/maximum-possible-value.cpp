1319b15502dc81dd518176b5383d160b51a77ae7
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-03-13 21:52:49 +0530

    Added solution - LeetHub

--- Maximum Possible Value - GFG/maximum-possible-value.cpp
+++ Maximum Possible Value - GFG/maximum-possible-value.cpp
@@ -8,17 +8,18 @@ using namespace std;
 class Solution {
   public:
     long long maxPossibleValue(int N,vector<int> A, vector<int> B) {
-        long long len , count , mn = INT_MAX , ans = 0 , sticks = 0;
+         long long len , count , mn = INT_MAX , ans = 0 , sticks = 0;
         for(int i = 0; i< N; i++){
-            len = A[i];
-            count = B[i];
+            len = A[i]; // length of the stick
+            count = B[i];//frequency
             //to find whether count is odd or not
-            if(count & 1 > 0)
-                count = count - 1;
+            if(count & 1 > 0) 
+                count = count - 1; //making the freq even
             if(count >= 2){
-                mn = min(mn , len);
+                mn = min(mn , len); //store min --> jishko baad mein 
+                //hataya ja skta hai
             }
-            ans += (len * count);
+            ans += (len * count);   //sum of perimeters
             sticks += count;
         }
         if(sticks%4 != 0){
