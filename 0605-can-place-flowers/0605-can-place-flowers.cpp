631e5813f52e0f8adba9ef9beac4aa4e15f06b79
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-03-20 19:21:21 +0530

    Time: 12 ms (93.73%), Space: 20.3 MB (34.30%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& arr, int n) {
-        if(n == 0)
+        if(n == 0){
             return true;
+        }
         for(int i = 0; i<arr.size(); i++){
             if(arr[i] == 0 and (i == 0 or arr[i-1] == 0) and (i == arr.size() - 1 or arr[i+1] == 0)){
                 arr[i] = 1;
