26ee5a75869e9a8546df6e49012164b64b07b266
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-03-30 09:03:35 +0530

    Added solution - LeetHub

--- Minimum Integer - GFG/minimum-integer.cpp
+++ Minimum Integer - GFG/minimum-integer.cpp
@@ -32,14 +32,16 @@ class Solution {
   public:
     int minimumInteger(int N, vector<int> &A) {
         long long int sum = 0;
-        for(int i = 0; i<N; i++)
-            sum += A[i];
-        int ans = INT_MAX;
         for(int i = 0; i<N; i++){
+            sum += A[i];
+        }
+        int ans = INT_MAX;
+        for(int i= 0; i<N;i++){
             int ele = A[i];
-            if(sum <= ( long long )N*ele){
+            if(sum <= (long long) N*ele){
                 ans = min(ans , ele);
             }
+            
         }
         return ans;
     }
