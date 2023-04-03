95386246a679a1566f9d97e7d30ad1eb5c531064
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-04-03 19:09:24 +0530

    Added solution - LeetHub

---
+++ Remainder on dividing by 11 - GFG/remainder-on-dividing-by-11.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+class Solution
+{
+
+public:
+    int xmod11(string x)
+    {
+       int n = x.length();
+        int sum = 0;
+        for(int i = 0; i<n; i++){
+            sum = (sum*10 + (x[i] - '0'))%11;
+        }
+        return sum;
+    
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string x;
+        cin>>x;
+        Solution ob;
+        int ans = ob.xmod11(x);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
