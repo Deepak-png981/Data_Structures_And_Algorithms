1472b44201afd274cf760c7caed7c38b0089ab62
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-04-10 21:40:49 +0530

    Time: 4 ms (39.51%), Space: 6.5 MB (12.09%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isValid(string s) {
-        int n = s.size();
+         int n = s.size();
         stack<int>st;
         for(int i = 0; i<n; i++){
             if(s[i] == '(' or s[i] == '{' or s[i] == '['){
