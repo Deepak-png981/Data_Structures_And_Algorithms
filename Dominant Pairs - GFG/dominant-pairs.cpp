ee5cc31504f4e3af96dccb7333aa5170834902f9
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-04-12 00:35:03 +0530

    Added solution - LeetHub

--- Dominant Pairs - GFG/dominant-pairs.cpp
+++ Dominant Pairs - GFG/dominant-pairs.cpp
@@ -11,15 +11,17 @@ using namespace std;
 class Solution{
 public:
     int dominantPairs(int n,vector<int> &arr){
-        sort(arr.begin(),arr.begin()+(n/2));
-        sort(arr.begin()+(n/2),arr.end());
+        //10 2 2 1
+        //2 10 1 2
+        sort(arr.begin() , arr.begin() + (n/2));
+        sort(arr.begin() + (n/2) , arr.end());
         int ans = 0;
         int right = n/2;
         for(int left = 0; left < n/2; left++){
-            while(right < n and arr[left] >= 5*arr[right]){
+            while(right < n and (arr[left] >= 5*arr[right])){
                 right++;
             }
-            ans += right-n/2;
+            ans += right - (n/2);
         }
         return ans;
     }  
