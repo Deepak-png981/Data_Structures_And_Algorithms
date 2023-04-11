dceeba412fdb311bbaf7601fa85dc69f557791a3
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-04-11 15:10:19 +0530

    Time: 524 ms (18.45%), Space: 178.6 MB (20.18%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,23 +1,19 @@
 class LRUCache {
-    
 public:
     class Node{
         public:
-        int key;
-        int val;
+        int key , val;
+        Node* prev;
         Node* next;
-        Node*prev;
-        Node(int key_ , int val_){
+        Node(int key_ , int value){
             key = key_;
-            val = val_;
+            val = value;
         }
-        
     };
     Node* head = new Node(-1 , -1);
-    Node* tail = new Node(-1,-1);
+    Node* tail = new Node(-1 , -1);
     int cap;
-    unordered_map<int , Node*>mp;
-    
+    unordered_map<int ,Node*> mp;
     LRUCache(int capacity) {
         cap = capacity;
         head->next = tail;
@@ -27,47 +23,40 @@ public:
         Node* temp = head->next;
         head->next = newnode;
         newnode->prev = head;
-        newnode->next = temp;
-        temp->prev = newnode;
-        
+        newnode -> next = temp;
+        temp -> prev = newnode;
     }
     void deleteNode(Node* delnode){
-        Node* delpre = delnode->prev;
+        Node* delprev = delnode->prev;
         Node* delnext = delnode->next;
-        delpre->next = delnext;
-        delnext->prev = delpre;
+        delprev->next = delnext;
+        delnext->prev = delprev;
     }
     int get(int key_) {
         if(mp.find(key_) != mp.end()){
             Node* resnode = mp[key_];
             int resval = resnode->val;
-            //value store kerne ke baad ush node ko 
-            //waha se erase kerke head ke kareeb create ker rhe hai
-            //taki LRU ban ske
+            //delete and insert
             mp.erase(key_);
             deleteNode(resnode);
             addNode(resnode);
             mp[key_] = head->next;
             return resval;
         }
-        //agar key nhi hai to return -1
         return -1;
-        
     }
     
     void put(int key_, int value) {
-        //agar key already exist ker rhi hai to
         if(mp.find(key_) != mp.end()){
-            Node* resval = mp[key_];
+            Node* resnode = mp[key_];
             mp.erase(key_);
-            deleteNode(resval);
+            deleteNode(resnode);
         }
-        //agar capacity or size barabad ho jaye to
         if(mp.size() == cap){
             mp.erase(tail->prev->key);
             deleteNode(tail->prev);
         }
-        //CREATING the new node
+        
         Node* newnode = new Node(key_ , value);
         addNode(newnode);
         mp[key_] = head->next;
