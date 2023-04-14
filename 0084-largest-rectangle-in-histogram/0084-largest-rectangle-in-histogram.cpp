a78a05662f1107407b69e5c96b6c06d11a8ae05d
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-04-14 22:36:28 +0530

    Time: 257 ms (14.18%), Space: 93.5 MB (16.46%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,44 +1,51 @@
 class Solution {
-    vector<int>nextSmallest(vector<int> heights , int n){
-        vector<int>ans(n);
+private:
+    vector<int> nextSmallerElement(vector<int>heights){
+        int n = heights.size();
+        vector<int>next(n);
         stack<int>st;
         st.push(-1);
         for(int i = n-1; i>=0; i--){
             int curr = heights[i];
             while(st.top() != -1 and heights[st.top()] >= curr)
-                st.pop();
-            ans[i] = st.top();
+                    st.pop();
+            next[i] = st.top();
             st.push(i);
         }
-        return ans;
+        return next;
     }
-    vector<int>prevSmallest(vector<int>heights , int n){
-        vector<int> ans(n);
+    
+    vector<int> prevSmallerElement(vector<int>heights){
+        int n = heights.size();
+        vector<int>prev(n);
         stack<int>st;
         st.push(-1);
-        for(int i = 0; i<n; i++){
-            int curr= heights[i];
+        for( int i = 0; i<n; i++ ){
+            int curr = heights[i];
             while(st.top() != -1 and heights[st.top()] >= curr)
-                st.pop();
-            ans[i] = st.top();
+                    st.pop();
+            prev[i] = st.top();
             st.push(i);
         }
-        return ans;
+        return prev;
     }
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
         vector<int>next(n);
-        next = nextSmallest(heights , n);
+        next = nextSmallerElement(heights);
         vector<int>prev(n);
-        prev = prevSmallest(heights, n);
-        
-        int mx = INT_MIN;
-        for(int i =0;i<n; i++){
+        prev = prevSmallerElement(heights);
+        int maxArea = INT_MIN;
+        for(int i = 0 ; i<n; i++){
+            
             if(next[i] == -1)
                 next[i] = n;
-            mx = max( mx , heights[i] * ( next[i] - prev[i] - 1 ));
+            int length = next[i] - prev[i] - 1;
+            int breadth = heights[i];
+            int newArea = length * breadth;
+            maxArea = max(maxArea , newArea);
         }
-        return mx;
+        return maxArea;
     }
 };
