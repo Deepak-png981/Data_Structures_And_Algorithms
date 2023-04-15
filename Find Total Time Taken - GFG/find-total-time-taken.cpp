1356df0d9890c879324e22eeb1c86a074906c633
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-04-15 10:30:06 +0530

    Added solution - LeetHub

---
+++ Find Total Time Taken - GFG/find-total-time-taken.cpp
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int totalTime(int n, vector<int> &arr, vector<int> &time) {
+        unordered_map<int , int>mp;
+        // mp[arr[0]] = time[0];
+        int totaltime = 0;
+        for(int i = 0; i<arr.size(); i++){
+            if(mp.find(arr[i]) == mp.end()){
+                mp[arr[i]] = totaltime;
+                
+            }else{
+                totaltime = max(totaltime , mp[arr[i]] + time[arr[i] - 1]);
+                mp[arr[i]] = totaltime;
+            }
+            totaltime++;
+            
+        }
+        return totaltime-1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        
+        
+        vector<int> arr(n);
+        Array::input(arr,n);
+        
+        
+        vector<int> time(n);
+        Array::input(time,n);
+        
+        Solution obj;
+        int res = obj.totalTime(n, arr, time);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
