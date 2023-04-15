0db45b0aa39c18a78709d4c4612af627d3f8399f
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-04-15 12:51:40 +0530

    Time: 260 ms (97.68%), Space: 134.8 MB (66.02%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
         deque<int>dq;
         vector<int>ans;
-        int n = nums.size();
         for(int i = 0; i<n; i++){
-            //agar index out of bond chale jaye use pop back
-            while(!dq.empty() and dq.front() == i-k)
+            while(!dq.empty() and dq.front() == i - k)
                 dq.pop_front();
-            
-            //chote elements to pop ker rhe hai because the
-            //dq should be in a decreasing fashion
             while(!dq.empty() and nums[dq.back()] <= nums[i])
-                    dq.pop_back();
+                dq.pop_back();
             dq.push_back(i);
-            if(i >= k-1){
+            if(i >= k-1)
                 ans.push_back(nums[dq.front()]);
-            }
         }
         return ans;
     }
