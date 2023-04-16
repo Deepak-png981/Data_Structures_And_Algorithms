2f1dc8b118e210b317ca681982b4a7310059abe6
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-04-16 18:41:34 +0530

    Added solution - LeetHub

--- Minimum steps to minimize n as per given condition - GFG/minimum-steps-to-minimize-n-as-per-given-condition.cpp
+++ Minimum steps to minimize n as per given condition - GFG/minimum-steps-to-minimize-n-as-per-given-condition.cpp
@@ -5,30 +5,31 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     private:
-    int minS(int n , vector<int>&ans){
-        if(n == 1){
-            ans[n] = 0;
-            return 0;
-        }
-        if(ans[n-1] == -1){
-            ans[n-1] = minS(n-1 , ans);
-        }
-        int y = INT_MAX , z = INT_MAX;
-        if(n % 2 == 0){
-            if(ans[n/2] == -1){
-                ans[n/2] = minS(n/2 , ans);
-            }
-            y = ans[n/2];
-        }
-        if(n % 3 == 0){
-            if(ans[n/3] == -1){
-                ans[n/3] = minS(n/3 , ans);
-            }
-            z = ans[n/3];
-        }
-        ans[n] = 1 + min(ans[n-1] , min( y , z));
-        return ans[n];
-    }
+    //Memoization ka function
+    // int minS(int n , vector<int>&ans){
+    //     if(n == 1){
+    //         ans[n] = 0;
+    //         return 0;
+    //     }
+    //     if(ans[n-1] == -1){
+    //         ans[n-1] = minS(n-1 , ans);
+    //     }
+    //     int y = INT_MAX , z = INT_MAX;
+    //     if(n % 2 == 0){
+    //         if(ans[n/2] == -1){
+    //             ans[n/2] = minS(n/2 , ans);
+    //         }
+    //         y = ans[n/2];
+    //     }
+    //     if(n % 3 == 0){
+    //         if(ans[n/3] == -1){
+    //             ans[n/3] = minS(n/3 , ans);
+    //         }
+    //         z = ans[n/3];
+    //     }
+    //     ans[n] = 1 + min(ans[n-1] , min( y , z));
+    //     return ans[n];
+    // }
 	public:
 	int minSteps(int n) 
 	{ 
@@ -46,8 +47,27 @@ class Solution{
 	   // return min( a , min( b , c));
 	   
 	   //MEMOIZATION USE
-	   vector<int>ans(n+1 ,-1);
-	   return minS(n , ans);
+	   //vector<int>ans(n+1 ,-1);
+	   //return minS(n , ans);
+	   
+	   //DP use kerke dikhayenge ab ishme
+	   vector<int>dp(n + 1 , INT_MAX);
+	   dp[0] = 0;
+	   dp[1] = 0;
+	   for(int i = 2; i<=n; i++){
+	       if(i%2 == 0){
+	           dp[i]  = 1 + min(dp[i/2] , dp[i-1]);
+	       }
+	       if(i%3 == 0){
+	           dp[i] = 1 + min(dp[i/3] , dp[i-1]);
+	       }
+	       else{
+	           dp[i] = min(dp[i] , 1 + dp[i-1]);
+	       }
+	   }
+	   return dp[n];
+	   
+	   
 	    
 	} 
 };
