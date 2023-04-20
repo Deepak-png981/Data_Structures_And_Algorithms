34ae8106cce74c5082dd7dcc7327cc11b7cb2d06
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-04-20 21:59:20 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (44.89%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -17,18 +17,16 @@ public:
         while(curr != NULL){
             if(curr -> left == NULL){
                 inorder.push_back(curr->val);
-                curr = curr -> right;
+                curr= curr->right;
             }
             else{
-                TreeNode* prev = curr->left;
-                while(prev->right != NULL and prev->right != curr){
+                TreeNode* prev = curr -> left;
+                while(prev -> right != NULL and prev->right != curr)
                     prev = prev->right;
-                }
                 if(prev->right == NULL){
-                    prev-> right = curr;
+                    prev->right = curr;
                     curr = curr->left;
-                }
-                else{
+                }else{
                     prev->right = NULL;
                     inorder.push_back(curr->val);
                     curr = curr->right;
