803f36ebc5f11c7facea32bc09530bda81047a51
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-04-22 10:10:37 +0530

    Updated Solution - GfG to GitHub

--- Medium/Smaller Sum/smaller-sum.cpp
+++ Medium/Smaller Sum/smaller-sum.cpp
@@ -9,23 +9,35 @@ using namespace std;
 class Solution{
 public:
     vector<long long> smallerSum(int n,vector<int> &arr){
+        //copy create
         vector<int>num(arr.begin() , arr.end());
+        //sum
         unsigned long long sum = 0;
-        for(int i = 0; i<arr.size(); i++){
-            sum += arr[i];
+        for(int i = 0; i<n; i++){
+            sum = sum + arr[i];
         }
+        //final vector
         vector<long long>result(n , 0);
-        //sorting in desc order
         sort(arr.begin() , arr.end() , greater<int>());
         unordered_map<int , long long>temp;
+        // 3 2 1
+        // sum 6
+        // 6 - 3 = 3 temp[3] = 3
+        // 3 - 2 = 1 temp[2] = 1
+        // 1 - 1 = 0 temp[1] = 0
         for(int i = 0; i<n; i++){
             sum = sum - arr[i];
             temp[arr[i]] = sum;
         }
+        //num = 1 2 3
+        //temp[1] 0
+        //temp[2] = 1
+        //temp[3] = 3
         for(int i = 0; i<n; i++){
             result[i] = temp[num[i]];
         }
         return result;
+        
     }
 };
 
