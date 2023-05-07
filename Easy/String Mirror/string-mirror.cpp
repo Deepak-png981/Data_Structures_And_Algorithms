4ab2f1a4b7c91531a6d4df59c23d901eac6ba015
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-05-07 14:53:24 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/String Mirror/string-mirror.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    string stringMirror(string str){
+        // Code here
+    string res;
+    res += str[0];
+    
+    for(int i=1;i<str.length();i++){
+        if(str[i] < str[i-1] || (i != 1 && str[i] == str[i-1]))res += str[i];
+        else break;
+    }
+    
+    string rev = res;
+    reverse(rev.begin(),rev.end());
+    
+    return res + rev;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string str;
+        cin>>str;
+        Solution ob;
+        string res=ob.stringMirror(str);
+        cout<<res<<endl;
+    }
+}
+// } Driver Code Ends
