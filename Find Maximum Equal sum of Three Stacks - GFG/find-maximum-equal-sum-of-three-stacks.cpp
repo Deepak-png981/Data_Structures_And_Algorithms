f9ac1d67a36f550b41180a365b3f701fda4cd594
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-06-03 20:30:37 +0530

    Added solution - LeetHub

--- Find Maximum Equal sum of Three Stacks - GFG/find-maximum-equal-sum-of-three-stacks.cpp
+++ Find Maximum Equal sum of Three Stacks - GFG/find-maximum-equal-sum-of-three-stacks.cpp
@@ -10,30 +10,48 @@ using namespace std;
 
 class Solution{
 public:
-    int maxEqualSum(int n1,int n2,int n3,vector<int> &s1,vector<int> &s2,vector<int> &s3){
-        // int sum1 = 0 , sum2 = 0 , sum3 = 0;
-        int sum1=accumulate(s1.begin(),s1.end(),0);
-        int sum2=accumulate(s2.begin(),s2.end(),0);
-        int sum3=accumulate(s3.begin(),s3.end(),0);
-        if(sum1  == sum2 and sum2 == sum3)
-            return sum1;
-        int i = 0 , j = 0 ,  k = 0;
-        while(i < n1 and j < n2 and k <n3){
-            if(sum1 == sum2 and sum2 == sum3)
-                return sum1;
-            sum1 -= s1[i];
-            i++;
-            while(sum1 < sum2){
-                sum2 -= s2[j];
-                j++;
-            }
-            while(sum1 < sum3){
-                sum3 -= s3[k];
-                k++;
-            }
-            
-        }
-        return 0;
+    int maxEqualSum(int N1,int N2,int N3,vector<int> &S1,vector<int> &S2,vector<int> &S3){
+       int sum1=0,sum2=0,sum3=0;
+       for(int i=0;i<N1;i++){
+           sum1+=S1[i];
+           
+       }
+      for(int i=0;i<N2;i++){
+           sum2+=S2[i];
+           
+       }
+       for(int i=0;i<N3;i++){
+           sum3+=S3[i];
+           
+       }
+       int i=0,j=0,k=0;
+       while(i<N1&&j<N2&&k<N3){
+           int min;
+           if(sum1<<sum2&&sum1<<sum3){
+               min=sum1;
+           }
+           else if(sum2<<sum1&&sum2<<sum3){
+               min=sum2;
+               
+           }
+           else{
+               min=sum3;
+           }
+           if(min==sum2&&min==sum3&&min==sum1)return min;
+           else if(sum1>sum2||sum1>sum3){
+               sum1-=S1[i];
+               i++;
+           }
+           else if(sum2>sum1||sum2>sum3){
+               sum2-=S2[j];
+               j++;
+           }
+           else if(sum3>sum1||sum3>sum2){
+               sum3-=S3[k];
+               k++;
+           }
+       }
+       return 0;
     }
 };
 
