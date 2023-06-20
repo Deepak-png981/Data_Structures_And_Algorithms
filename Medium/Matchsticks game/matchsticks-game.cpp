2c37b448987f169ff1ee647e06c061f639f71192
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-06-20 08:54:44 +0530

    Updated Solution - GfG to GitHub

--- Medium/Matchsticks game/matchsticks-game.cpp
+++ Medium/Matchsticks game/matchsticks-game.cpp
@@ -6,22 +6,11 @@ using namespace std;
 class Solution {
   public:
     int matchGame(long long N) {
-        //1 A
-        //2 A
-        //3 A
-        //4 A
-        //5 A1 B4 -> B
-        //6 A4 B1 A1 -> A
-        //7 A1 B4 A2 -> A
-        //8 A4 B1 A3 -> A
-        //9 A1 B4 A4 -> A
-        //10 A4 B1 A4 B1 -> B
-        //13 A1 B4 A1 B4 A3 -> A
-        //15 A1 B4 A1 B4 A4 B1 -> B
-        if(N%5 == 0)
+        // code here
+          if(N%5 == 0)
             return -1;
-        else
-         return N%5;
+            else
+             return N%5;
     }
 };
 
