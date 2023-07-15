4d4d2e2b72386fa1977c378f0330eb8270943a22
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-07-15 22:17:06 +0530

    Time: 235 ms (14.64%), Space: 83.9 MB (18.11%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        //size of nums
+        unordered_map<int, int>map;
         int n = nums.size();
-        for(int i = 0 ; i <n; i++){
-            //(nums[i]finding the index so that I can multiply by -1 further
-            int index = abs(nums[i]) - 1;
-            //multiplying the nums[index] with -1 , if it is occuring two times
-            //then it will automatically convert into positive
-            nums[index] *= -1;
-            //if it is positive then it is occuring two times mere bhai
-            if(nums[index] >= 0)
-                return abs(nums[i]);            
+        for(int i = 0; i<n;i++)
+        {    
+            map[nums[i]]++;
+            if(map[nums[i]] > 1)
+                return nums[i];
         }
         return -1;
-                 
     }
 };
