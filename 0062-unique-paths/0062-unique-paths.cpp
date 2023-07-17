e168a86b7e27f95ff99ce99267f4aefb44ad91b2
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-07-17 22:22:50 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (48.83%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,20 +1,24 @@
 class Solution {
+private:
     int countPaths(int i , int j , int m , int n , vector<vector<int>>&dp){
+        //base case
         if(i == m-1 and j == n-1)
             return 1;
         if(i >= m or j >= n)
             return 0;
         if(dp[i][j] != -1)
             return dp[i][j];
-        else return dp[i][j] = countPaths(i + 1 , j , m , n ,dp) + countPaths(i , j+1 , m , n ,dp);
+        int rowmove = countPaths(i + 1 , j , m , n,dp);
+        int colmove = countPaths(i , j+1 , m , n,dp);
+        return  dp[i][j]=rowmove  + colmove;
     }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m + 1 , vector<int>(n+1 , -1));
-        return countPaths(0 , 0 , m , n,dp);
+        vector<vector<int>>dp(m+1 , vector<int>(n+1 , -1));
+        
+        return countPaths( 0, 0 , m , n ,dp);
         if(m == 1 and n == 1)
             return 1;
         return dp[0][0];
-        
     }
 };
