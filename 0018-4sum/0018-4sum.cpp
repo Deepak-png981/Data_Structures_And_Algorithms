a7a73d40a63b521cf9ea46a675f3759e8f2b3807
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-07-21 22:48:09 +0530

    Time: 2846 ms (5.02%), Space: 503.8 MB (5.34%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,44 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-         vector<vector<int>> res;
-    
-    if(nums.empty()) return res;
-    
-    int n = nums.size();
-    sort(nums.begin(),nums.end());
-    
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            
-            long target2 = long(target) - long(nums[j]) - long(nums[i]);
-            int left = j+1;
-            int right = n-1;
-            
-            while(left < right){
-                long twoSum = nums[left] + nums[right];
-                if(left < right && twoSum < target2){
-                    left++;
-                }else if(left < right && twoSum > target2){
-                    right--;
-                }else{
-                    vector<int> storeAns(4,0);
-                    storeAns[0] = nums[i];
-                    storeAns[1] = nums[j];
-                    storeAns[2] = nums[left];
-                    storeAns[3] = nums[right];
-                    res.push_back(storeAns);
-                    
-                   while(left < right && nums[left] == storeAns[2]) ++left;
-                   while(left < right && nums[right] == storeAns[3]) --right;                     
-
+    vector<vector<int>> fourSum(vector<int>& arr, int target) {
+        int n = arr.size();
+        set<vector<int>>st;
+        for(int i = 0; i<n; i++){
+            for(int j = i + 1 ; j<n; j++){
+                set<long long>hashset;
+                for(int k = j + 1 ; k<n; k++){
+                    long long sum = arr[i] + arr[j];
+                    sum += arr[k];
+                    long long fourth = target - sum;
+                    if(hashset.find(fourth) != hashset.end()){
+                        vector<int>temp = {arr[i] , arr[j] , arr[k], (int)fourth};
+                        sort(temp.begin() , temp.end());
+                        st.insert(temp);
+                       
+                    }
+                     hashset.insert(arr[k]);
                 }
             }
-            while(j+1 < n && nums[j+1] == nums[j]) ++j;
         }
-        while(i+1 < n && nums[i+1] == nums[i]) ++i;
-    }
-    
-    return res;
+        vector<vector<int>>ans(st.begin() , st.end());
+        return ans;
     }
 };
