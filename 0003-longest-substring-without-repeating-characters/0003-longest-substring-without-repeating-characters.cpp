bbaca23bfcc427645a2e1738cc8231e3447fa3d8
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-07-25 14:41:18 +0530

    Time: 57 ms (15.45%), Space: 12 MB (19.39%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        int ans = 0;
+        for(int i = 0;i < n; i++){
+            vector<bool>vis(256 , false);
+            for(int j = i; j<n; j++){
+                if(vis[s[j]] == true)
+                    break;
+                else{
+                    ans = max(ans , j - i + 1);
+                    vis[s[j]] = true;
+                }
+            }
+            vis[s[i]] = false;
+        }
+        return ans;
+    }
+};
