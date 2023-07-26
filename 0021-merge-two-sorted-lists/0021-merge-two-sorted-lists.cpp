30e26ec34254291d6520c2f666d119e57475fab2
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-07-26 23:23:58 +0530

    Time: 8 ms (71.45%), Space: 14.8 MB (19.78%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -1,57 +1,48 @@
 /**
- *Definition for singly-linked list.
- *struct ListNode {
- *   int val;
- *   ListNode * next;
- *   ListNode() : val(0), next(nullptr) {}
- *   ListNode(int x) : val(x), next(nullptr) {}
- *   ListNode(int x, ListNode *next) : val(x), next(next) {}
- *};
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
  */
-class Solution
-{
-    public:
-        ListNode* mergeTwoLists(ListNode *list1, ListNode *list2)
-        {
-            if (list1 == NULL)
-                return list2;
-            if (list2 == NULL)
-                return list1;
-            ListNode *ans = NULL;
-           
-            if (list1->val > list2->val)
-            {
-                ans = list2;
-                list2 = list2->next;
-            }
-            else
-            {
-                ans = list1;
-                list1 = list1->next;
-            }
-            ListNode * res = ans;
-            while (list1 != NULL and list2 != NULL)
-            {
+class Solution {
+public:
 
-                if (list1->val > list2->val)
-                {
-                    ans->next = list2;
-                    list2 = list2->next;
-                }
-                else
-                {
-                    ans->next = list1;
-                    list1 = list1->next;
-                }
-                ans = ans->next;
-            }
-            if (list1 != NULL)
-            {
-                ans->next = list1;
-            }
-            else
-                ans->next = list2;
-
-            return res;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        
+        if(list1 == NULL)
+            return list2;
+        if(list2 == NULL)
+            return list1;
+        ListNode* ans;
+        if(list1 -> val > list2->val){
+            ans = list2;
+            list2 = list2->next;
+        }else{
+            ans = list1;
+            list1 = list1->next;
         }
+        ListNode* res = ans;
+        while(list1 != NULL and list2 != NULL){
+            if(list1 -> val > list2->val){
+                ans->next = list2;
+                list2 = list2->next;
+               
+            }else{
+                ans->next = list1;
+                list1 = list1->next;
+               
+            }
+             ans = ans->next;
+        }
+        if(list1 != NULL){
+            ans->next = list1;
+        }
+        if(list2 != NULL)
+            ans -> next = list2;
+        return res;
+    }
 };
