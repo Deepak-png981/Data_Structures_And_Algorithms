1bd2c614361ba74c274b438c07b7ab5ae0b755d8
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-07-26 22:36:22 +0530

    Time: 6 ms (78.89%), Space: 14.8 MB (53.97%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -1,41 +1,57 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *   int val;
+ *   ListNode * next;
+ *   ListNode() : val(0), next(nullptr) {}
+ *   ListNode(int x) : val(x), next(nullptr) {}
+ *   ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
  */
-class Solution {
-public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1 == NULL)   return list2;
-        if(list2 == NULL)   return list1;
-        ListNode* ptr = list1;
-        if(list1 -> val > list2 -> val){
-            ptr = list2;
-            list2 = list2->next;
-        }else{
-            list1 = list1->next;
-        }
-        ListNode* ans = ptr;
-        while(list1 and list2){
-            if(list1 -> val > list2->val){
-                ans->next = list2;
+class Solution
+{
+    public:
+        ListNode* mergeTwoLists(ListNode *list1, ListNode *list2)
+        {
+            if (list1 == NULL)
+                return list2;
+            if (list2 == NULL)
+                return list1;
+            ListNode *ans = NULL;
+           
+            if (list1->val > list2->val)
+            {
+                ans = list2;
                 list2 = list2->next;
-            }else{
-                ans->next = list1;
+            }
+            else
+            {
+                ans = list1;
                 list1 = list1->next;
             }
-            ans = ans->next;
+            ListNode * res = ans;
+            while (list1 != NULL and list2 != NULL)
+            {
+
+                if (list1->val > list2->val)
+                {
+                    ans->next = list2;
+                    list2 = list2->next;
+                }
+                else
+                {
+                    ans->next = list1;
+                    list1 = list1->next;
+                }
+                ans = ans->next;
+            }
+            if (list1 != NULL)
+            {
+                ans->next = list1;
+            }
+            else
+                ans->next = list2;
+
+            return res;
         }
-        //dono mein se ek end ho gyi to ab agli ko direct connect kerdo
-        if(!list1)
-                ans -> next = list2;
-        else
-                ans -> next = list1;
-        return ptr;
-    }
 };
