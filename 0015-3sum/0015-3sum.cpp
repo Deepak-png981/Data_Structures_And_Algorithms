5bc332060d87e6031b41287c1254096a8cb7be20
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-08-01 19:18:40 +0530

    Time: 118 ms (94.46%), Space: 25.8 MB (58.70%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // sorting the array
+        vector<vector<int>>ans;
         sort(nums.begin() , nums.end());
-        //size 3 se chota hua to kaise chalega
-        if(nums.size() < 3) return {};
-        //agar sorted array ka phla element +ve nikla to kaise chalega babu bhaiya
-        if(nums[0] > 0) return{};
-        vector<vector<int>> ans;
-        for(int i = 0 ; i<nums.size(); i++){
-            if(nums[i] > 0)  //+ve elements milne shuru hogye S.A mein to zero nhi banaskte
-                break;
-            if(i>0 and nums[i] == nums[i-1]) // duplicate niklenge to continue kero bhaiya
+        int n = nums.size();
+        for(int i = 0; i<n; i++){
+            if(i != 0 and nums[i] == nums[i-1])
                 continue;
-            //do pointers banao low and high or sum ko 0 se initialize kero 
-            int low = i+1 , high = nums.size() - 1 , sum = 0;
-            while(low < high){  //Search using two pointers, binary search ki trha
-                sum = nums[i] + nums[low] + nums[high]; // sum nikala hai
-                if(sum > 0)
-                    high--;
-                else if(sum < 0)
-                    low++;
-                else{   //hume wo teeno mil gye hai
-                    ans.push_back({nums[i] , nums[low] , nums[high]});
-                    //ab ye elements duplicate na ho isliye ish case ko handle ker rhe hai
-                    int low_last = nums[low] , high_last = nums[high];
-                    //duplicate na aye isiliye main low or high ko last occurence of nums[low] and nums[high]
-                    //tak chodh ker aunga
-                    while(low < high and nums[low] == low_last) 
-                        low++;
-                    while(low < high and nums[high] == high_last)
-                        high--;
+            int j = i+1;
+            int k = n-1;
+            while(j < k){
+               int sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0){
+                    j++;
+                }else if(sum > 0){
+                    k--;
+                }else{
+                    vector<int>temp = {nums[i] , nums[j] , nums[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    while(j < k and nums[j] == nums[j-1]) j++;
+                    while(j < k and nums[k] == nums[k+1]) k--;
                 }
             }
         }
