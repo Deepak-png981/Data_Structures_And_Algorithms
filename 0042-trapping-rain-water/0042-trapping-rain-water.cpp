6736f289404bd0aec9689f84300f5ef85080c3d1
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-08-02 12:59:21 +0530

    Time: 15 ms (76.36%), Space: 19.9 MB (45.83%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int trap(vector<int>& arr) {
+       //brute force
         int n = arr.size();
-        int maxleft = 0 , maxright = 0;
-        int left = 0 , right = n-1;
         int ans = 0;
-        while(left <= right){
-            if(arr[left] <= arr[right]){
-                if(arr[left] >= maxleft)
-                    maxleft = arr[left];
-                else
-                    ans += maxleft - arr[left];
-                left++;
-            }else{
-                if(arr[right] >= maxright){
-                    maxright = arr[right];
-                }else{
-                    ans += maxright - arr[right];
-                }
-                right--;
-            }
+        int prefix[n] , suffix[n];
+        //calculating prefix[i]
+        prefix[0] = arr[0];
+        for(int i = 1; i<n; i++){
+            prefix[i] = max(prefix[i-1] , arr[i]);
+        }
+        //calculating suffix[i]
+        suffix[n-1] = arr[n-1];
+        for(int i = n-2; i>=0; i--){
+            suffix[i] = max(suffix[i+1] , arr[i]);
+        }
+        //final step
+        for(int i = 0; i<n; i++){
+            ans += min(prefix[i] , suffix[i]) - arr[i];
         }
         return ans;
     }
