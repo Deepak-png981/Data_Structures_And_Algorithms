ab9e0bb94f53e86c8cdd2dff87b9816d0ed615fd
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-08-03 14:04:10 +0530

    Added solution - LeetHub

--- N meetings in one room - GFG/n-meetings-in-one-room.cpp
+++ N meetings in one room - GFG/n-meetings-in-one-room.cpp
@@ -5,25 +5,26 @@ using namespace std;
 // } Driver Code Ends
 class Solution
 {
+    private:
+    static bool cmp(pair<int , int>a , pair<int , int>b){
+        return a.second < b.second;
+    }
     public:
     //Function to find the maximum number of meetings that can
     //be performed in a meeting room.
-    static bool cmp(pair<int, int> f , pair<int , int>s){
-        return s.second > f.second;
-    }
     int maxMeetings(int start[], int end[], int n)
     {
-        vector<pair<int , int>> p;
-        for(int i = 0; i<n ; i++){
-            p.push_back({start[i] , end[i]});
+        vector<pair<int , int>>v;
+        for(int i = 0; i<n; i++){
+            v.push_back({start[i] , end[i]});
         }
-        sort(p.begin() , p.end() , cmp);
-        int ansEnd = p[0].second;
+        sort(v.begin() , v.end() , cmp);
         int count = 1;
+        int ansEnd = v[0].second;
         for(int i = 1; i<n; i++){
-            if(p[i].first > ansEnd){
+            if(v[i].first > ansEnd){
+                ansEnd = v[i].second;
                 count++;
-                ansEnd = p[i].second;
             }
         }
         return count;
