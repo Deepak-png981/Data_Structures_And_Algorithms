1b6bce69989f39e4b94c01bed936ad0cce49d2d8
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-08-07 11:11:42 +0530

    Added solution - LeetHub

--- Job Sequencing Problem - GFG/job-sequencing-problem.cpp
+++ Job Sequencing Problem - GFG/job-sequencing-problem.cpp
@@ -33,32 +33,29 @@ class Solution
     //Function to find the maximum profit and the number of jobs done.
     vector<int> JobScheduling(Job arr[], int n) 
     { 
-        vector<int>ans;
         sort(arr , arr + n , cmp);
-        
-        //calculating the maximum deadline
-        int maxi = arr[0].dead;
-        for(int i = 1; i<n; i++){
-            maxi = max(arr[i].dead  , maxi);
-        }
-        int slot[maxi + 1];
-        for(int i = 0; i<=maxi; i++)
-            slot[i] = -1;
-        int countJob = 0 , profitJob = 0;
+        int maxiDead = INT_MIN;
         for(int i = 0; i<n; i++){
-            for(int j = arr[i].dead ; j>0; j--){
-                if(slot[j] == -1){
-                    slot[j] = i;
-                    countJob++;
-                    profitJob += arr[i].profit;
+            maxiDead = max(maxiDead , arr[i].dead);
+        }
+        int deadArray[maxiDead + 1];
+        for(int i = 0; i<= maxiDead; i++)
+            deadArray[i] = -1;
+        int countJobs = 0 , jobProfit = 0;
+        for(int i = 0; i<n; i++){
+            for(int j = arr[i].dead; j>0; j--){
+                if(deadArray[j] == -1){
+                    countJobs += 1;
+                    jobProfit += arr[i].profit;
+                    deadArray[j] = arr[i].id;
                     break;
                 }
             }
         }
-        ans.push_back(countJob);
-        ans.push_back(profitJob);
+        vector<int>ans;
+        ans.push_back(countJobs);
+        ans.push_back(jobProfit);
         return ans;
-        
     } 
 };
 
