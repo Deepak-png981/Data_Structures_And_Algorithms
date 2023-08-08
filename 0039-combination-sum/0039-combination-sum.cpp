d76aeba476f826f266b251fe65703405e9460f3b
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-08-08 14:17:08 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (65.24%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,30 +1,27 @@
 class Solution {
+private:
+    void helper(int index , vector<int>&ds , vector<vector<int>>&ans ,int target , vector<int>&candidates){
+        if(index == candidates.size()){
+               if(target == 0){
+                   ans.push_back(ds);
+               }
+            return;                  
+        }
+        //pick up the element
+        if(candidates[index] <= target){
+            ds.push_back(candidates[index]);
+            helper(index , ds , ans , target - candidates[index] , candidates);
+            ds.pop_back();
+        }
+        //not picking up the element
+        helper(index + 1 , ds , ans , target , candidates);
+    }
 public:
-    vector<vector<int>>ans;
-    void helper(int i , vector<int>& cand , int target , vector<int>&res){
-        //base case
-        if(target == 0){
-            ans.push_back(res);
-            return ;
-        }
-        //if the target becomes negative
-        if(target<0){
-            return;
-        }
-        //if the have come to end of the array and surely we haven't found out that the value of target == 0
-        if(i == cand.size()){
-            return;
-        }
-        //if we are skipping the first element
-        helper(i+1 , cand , target , res);
-        //if we are considering the first element then
-        res.push_back(cand[i]);
-        helper(i , cand , target - cand[i] , res);
-        res.pop_back(); //backtracking
-    }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int>res;
-        helper(0 , candidates , target , res);
+        vector<vector<int>>ans;
+        vector<int>ds;
+        helper(0 , ds , ans , target , candidates);
         return ans;
-    }
+        
+}
 };
