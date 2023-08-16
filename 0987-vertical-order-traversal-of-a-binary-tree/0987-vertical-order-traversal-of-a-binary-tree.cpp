cf1adc1192dc8e669a6572a07def86d3ab259d1e
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-08-16 14:11:49 +0530

    Time: 3 ms (85.69%), Space: 13.6 MB (46.34%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,62 +1,60 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    vector<vector<int>> verticalTraversal(TreeNode* root) {
-  map < int, map < int, multiset < int >>> nodes;
-  queue < pair < TreeNode * , pair < int, int >>> todo;
-  todo.push({
-    root,
-    {
-      0,
-      0
-    }
-  }); //initial vertical and level
-  while (!todo.empty()) {
-    auto p = todo.front();
-    todo.pop();
-    TreeNode * temp = p.first;
-
-    //x -> vertical , y->level
-    int x = p.second.first, y = p.second.second;
-    nodes[x][y].insert(temp -> val); //inserting to multiset
-
-    if (temp -> left) {
-      todo.push({
-        temp -> left,
+class Solution
+{
+    public:
+        vector<vector < int>> verticalTraversal(TreeNode *root)
         {
-          x - 1,
-          y + 1
+            map<int, map<int, multiset< int>>> mp;	//row, col, data
+            queue<pair<TreeNode*, pair<int, int>>> q;
+            q.push({ root,
+                { 0,
+                    0 } });
+            while (!q.empty())
+            {
+                auto it = q.front();
+                q.pop();
+
+                TreeNode *node = it.first;
+                int x = it.second.first;
+                int y = it.second.second;
+                mp[x][y].insert(node->val);
+                if (node->left)
+                {
+                    q.push({ node->left,
+                        {
+                            x - 1,
+                            y + 1
+                        } });
+                }
+                if (node->right)
+                {
+                    q.push({ node->right,
+                        {
+                            x + 1,
+                            y + 1
+                        } });
+                }
+            }
+            vector<vector < int>> ans;
+            for (auto p: mp)
+            {
+                vector<int> col;
+                for (auto q: p.second)
+                {
+                    col.insert(col.end(), q.second.begin(), q.second.end());
+                }
+                ans.push_back(col);
+            }
+            return ans;
         }
-      });
-    }
-    if (temp -> right) {
-      todo.push({
-        temp -> right,
-        {
-          x + 1,
-          y + 1
-        }
-      });
-    }
-  }
-  vector < vector < int >> ans;
-  for (auto p: nodes) {
-    vector < int > col;
-    for (auto q: p.second) {
-      col.insert(col.end(), q.second.begin(), q.second.end());
-    }
-    ans.push_back(col);
-  }
-  return ans;
-}
 };
