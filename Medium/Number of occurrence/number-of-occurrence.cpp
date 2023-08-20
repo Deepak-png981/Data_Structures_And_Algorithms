7005b6fe80edf9c0ffe619dd137099d82d572038
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-08-20 21:46:34 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number of occurrence/number-of-occurrence.cpp
+++ Medium/Number of occurrence/number-of-occurrence.cpp
@@ -10,7 +10,7 @@ class Solution{
 public:	
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
-  int count(int arr[], int n, int x) {
+	int count(int arr[], int n, int x) {
         return upper_bound(arr, arr+n, x) - lower_bound(arr, arr+n, x) ;}
 };
 
