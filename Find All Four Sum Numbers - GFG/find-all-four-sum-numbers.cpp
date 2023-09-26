4afd5c0bc4983617821202f131f6d18671452aac
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-09-26 21:44:45 +0530

    Added solution - LeetHub

--- Find All Four Sum Numbers - GFG/find-all-four-sum-numbers.cpp
+++ Find All Four Sum Numbers - GFG/find-all-four-sum-numbers.cpp
@@ -10,37 +10,45 @@ class Solution{
     public:
     // arr[] : int input array of integers
     // k : the quadruple sum required
-    vector<vector<int> > fourSum(vector<int> &arr, int target) {
-        int n = arr.size();
-        sort(arr.begin() , arr.end());
-        vector<vector<int>>ans;
-        for(int i = 0; i<n; i++){
-            if(i > 0 and arr[i] == arr[i-1])
-                continue;
-            for(int j = i + 1; j<n; j++){
-                if( j > i+1 and arr[j] == arr[j-1])
-                    continue;
-                int k = j + 1;
-                int l = n-1;
-                while(k < l){
-                    long long sum = arr[i];
-                    sum += arr[j];
-                    sum += arr[k];
-                    sum += arr[l];
-                    if(sum == target){
-                        vector<int>temp = {arr[i] , arr[j] ,arr[k] ,arr[l]};
-                        ans.push_back(temp);
-                        k++,l--;
-                        while(k < l and arr[k] == arr[k-1]) k++;
-                        while(k < l and arr[l] == arr[l + 1])l--;
-                    }else if(sum < target){
-                        k++;
-                    }else 
-                        l--;
+    vector<vector<int> > fourSum(vector<int>& nums, int target) {
+    int n = nums.size(); //size of the array
+    vector<vector<int>> ans;
+
+    // sort the given array:
+    sort(nums.begin(), nums.end());
+
+    //calculating the quadruplets:
+    for (int i = 0; i < n; i++) {
+        // avoid the duplicates while moving i:
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+        for (int j = i + 1; j < n; j++) {
+            // avoid the duplicates while moving j:
+            if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+            // 2 pointers:
+            int k = j + 1;
+            int l = n - 1;
+            while (k < l) {
+                long long sum = nums[i];
+                sum += nums[j];
+                sum += nums[k];
+                sum += nums[l];
+                if (sum == target) {
+                    vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                    ans.push_back(temp);
+                    k++; l--;
+
+                    //skip the duplicates:
+                    while (k < l && nums[k] == nums[k - 1]) k++;
+                    while (k < l && nums[l] == nums[l + 1]) l--;
                 }
+                else if (sum < target) k++;
+                else l--;
             }
         }
-        return ans;
+     }
+
+      return ans;
     }
 };
 
