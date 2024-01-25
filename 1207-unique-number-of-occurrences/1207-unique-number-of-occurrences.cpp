dd749db6b87778a641818ce530e37b68ba819a22
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2024-01-25 10:14:34 +0530

    Time: 3 ms (47.55%), Space: 10.9 MB (5.86%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,12 +1,14 @@
-class Solution 
-{
+class Solution {
 public:
-    bool uniqueOccurrences(vector<int>& arr) 
-    {
-        unordered_map<int, int> cnt;
-        unordered_set<int>      unq;
-        for (int n   : arr) cnt[n]++;               // [1] count frequencies
-        for (auto& c : cnt) unq.insert(c.second);   // [2] collect unique frequencies
-        return cnt.size() == unq.size();
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int , int>mp;
+        unordered_set<int>cnt;
+        for(auto n : arr){
+            mp[n]++;
+        }
+        for(auto c : mp){
+            cnt.insert(c.second);
+        }
+        return cnt.size() == mp.size();
     }
 };
