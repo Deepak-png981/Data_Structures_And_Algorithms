14eb487e86c8cb04ae8b6b5431f5efd5b9d6075a
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2024-06-03 00:08:55 +0530

    Time: 51 ms (97.46%), Space: 53.2 MB (94.45%) - LeetHub

--- 0169-majority-element/0169-majority-element.ts
+++ 0169-majority-element/0169-majority-element.ts
@@ -1,14 +1,13 @@
 function majorityElement(nums: number[]): number {
-    
-    const size = nums.length;
-    let count = 0 , element : number;
-    for(let i = 0; i<size; i++){
+    const n = nums.length;
+    let element : number;
+    let count = 0;
+    for(let i = 0; i<n; i++){
         if(count == 0){
-            count++;
             element = nums[i];
-        }else if(element == nums[i]) count++;
-        else count--;
+            count++;
+        }else if(element == nums[i])    count++;
+        else    count--;
     }
     return element;
-    
 };
