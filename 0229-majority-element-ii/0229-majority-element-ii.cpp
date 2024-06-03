89e92f1381ff0bc60c1961817a9120b2c77c57d6
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2024-06-03 08:53:19 +0530

    Time: 3 ms (98.23%), Space: 18.3 MB (91.72%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        map<int, int>mp;
+        int count1 = 0 , count2 = 0;
+        int element1 = INT_MIN , element2 = INT_MIN;
         int n = nums.size();
-        int count = n/3 + 1;
-        vector<int>ans;
         for(int i = 0; i<n; i++){
-            mp[nums[i]]++;
-            if(mp[nums[i]] == count){
-                ans.push_back(nums[i]);
+            if(count1 == 0 and nums[i] != element2){
+                count1++;
+                element1 = nums[i];
+            }else if(count2 == 0 and nums[i] != element1){
+                count2++;
+                element2 = nums[i];
+            }else if(element1 == nums[i])   count1++;
+            else if(element2 == nums[i])    count2++;
+            else {
+                count1--;
+                count2--;
             }
-            if(ans.size() == 2)
-                return ans;
         }
+        count1 = 0 , count2 = 0;
+        for(int i = 0; i<n;i++){
+            if(nums[i] == element1)
+                count1++;
+            else if(nums[i] == element2)
+                count2++;
+        }
+        vector<int>ans;
+        if(count1 > n/3)
+            ans.push_back(element1);
+        if(count2 > n/3)
+            ans.push_back(element2);
         return ans;
     }
 };
