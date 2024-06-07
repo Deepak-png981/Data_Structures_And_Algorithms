f5fc57b17c1f6baab00fbe86cc31827c4b823479
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2024-06-07 23:05:21 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (63.06%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,24 +1,21 @@
 class Solution {
 private:
-    int countPaths(int i , int j , int m , int n , vector<vector<int>>&dp){
-        //base case
-        if(i == m-1 and j == n-1)
+    int recur(int i , int j , int n , int m, vector<vector<int>>&dp){
+        //if the robot is at the bottom right
+        if( i == (n-1) and j == (m-1))
             return 1;
-        if(i >= m or j >= n)
+        if( i>=n or j>=m)
             return 0;
         if(dp[i][j] != -1)
             return dp[i][j];
-        int rowmove = countPaths(i + 1 , j , m , n,dp);
-        int colmove = countPaths(i , j+1 , m , n,dp);
-        return  dp[i][j]=rowmove  + colmove;
+        else 
+            return dp[i][j] = recur(i + 1 , j , n , m,dp) + recur(i , j+1 , n , m,dp);
+        
     }
 public:
-    int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m+1 , vector<int>(n+1 , -1));
-        
-        return countPaths( 0, 0 , m , n ,dp);
-        if(m == 1 and n == 1)
-            return 1;
-        return dp[0][0];
+    int uniquePaths(int n, int m) {
+        vector<vector<int>>dp(n , vector<int>(m,-1));
+        int count = recur(0 , 0 ,n, m,dp);
+        return count;
     }
 };
