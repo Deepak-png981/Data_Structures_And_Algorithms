c5945db5f68c14998e3782289063f430e6801195
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2024-11-01 22:29:49 +0530

    Time: 12 ms (81.39%), Space: 53 MB (89.28%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -0,0 +1,22 @@
+function isAnagram(s: string, t: string): boolean {
+    if(s.length !== t.length){
+        return false;
+    }
+    const obj : Record<string , number> = {};
+    for(let i = 0; i<s.length; i++){
+        const s_char = s[i];
+        const t_char = t[i];
+        if(obj[s_char] === undefined)   obj[s_char] = 0;
+        if(obj[t_char] === undefined)   obj[t_char] = 0;
+        
+        obj[s_char]++;
+        obj[t_char]--;
+    }
+    const keys: string[] = Object.keys(obj);
+    for(const key of keys){
+        const value = obj[key];
+        if(value != 0)
+            return false;
+    }
+    return true;
+};
