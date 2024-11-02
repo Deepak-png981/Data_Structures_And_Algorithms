aed6fcbf6e156e5c12266607a5dcdc984176e97d
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2024-11-02 22:45:15 +0530

    Time: 2 ms (87.63%), Space: 53.3 MB (17.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,13 +1,11 @@
 function twoSum(nums: number[], target: number): number[] {
-    const mp : Map<number , number> = new Map();
-    for(let i = 0; i<nums.length; i++){
-        mp.set(nums[i] , i);
-    }
+    const mp: Map<number , number> = new Map();
     for(let i = 0; i<nums.length; i++){
         const complement = target - nums[i];
-        if(mp.has(complement) && mp.get(complement) != i ){
-            return [i , mp.get(target -nums[i])];
+        if(mp.has(complement)){
+            return [i , mp.get(complement)];
         }
+        mp.set(nums[i] , i);
     }
     return [];
 };
