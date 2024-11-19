5720adcb0c8592abbe8c3d3819bd1030c8108b37
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2024-11-19 15:42:58 +0530

    Time: 0 ms (100.00%), Space: 11.8 MB (94.92%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,35 +1,35 @@
 class Solution {
-private:
-    bool isValid(string str){
-        if(str[0] == ')')
-            return false;
-        stack<char>st;
-        int n = str.size();
-        for(int i = 0; i<n; i++){
-            if(str[i] == '('){
-                st.push(str[i]);
-            }else if(st.empty() and str[i] == ')')
-                return false;
-            else if(str[i] == ')')
-                st.pop();
+private: 
+    void backtrack(
+        int openN , 
+        int closedN ,
+        int n ,
+        vector<string>&result , 
+        string& stack
+    )
+    {
+        if(openN == closedN and openN == n){
+            result.push_back(stack);
+            return;
         }
-        return st.empty();
+        if(openN < n){
+            stack += '(';
+            backtrack(openN + 1 , closedN , n , result , stack);
+            stack.pop_back();
+        }
+        if(closedN < openN){
+            stack += ')';
+            backtrack(openN , closedN+1 , n , result , stack);
+            stack.pop_back();
+        }
+        
+        
     }
 public:
     vector<string> generateParenthesis(int n) {
-        string st = "";
         vector<string>ans;
-        for(int i = 0; i<n; i++){
-            st += '(';
-        }
-        for(int i = 0; i<n; i++){
-            st += ')';
-        }
-        ans.push_back(st);
-        while(next_permutation(st.begin() , st.end())){
-            if(isValid(st))
-                ans.push_back(st);
-        }
+        string s;
+        backtrack(0 , 0 , n , ans, s);
         return ans;
     }
 };
