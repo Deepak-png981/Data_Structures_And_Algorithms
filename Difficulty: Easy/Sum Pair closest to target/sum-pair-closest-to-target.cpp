c12cdabd34d45e698f4310ae46df8002d5677621
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2025-01-06 18:33:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum Pair closest to target/sum-pair-closest-to-target.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    vector<int> sumClosest(vector<int>& arr, int target) {
+        vector<int> res;
+        int closest_diff=1e9;
+        sort(arr.begin(),arr.end());
+        int start=0, end=arr.size()-1;
+        while(start<end){
+            int sum=arr[start]+arr[end];
+            int d=abs(target-sum);
+            if(d<closest_diff){
+                closest_diff=d;
+                res={arr[start],arr[end]};
+            }
+            if(sum<=target) start++;
+            else end--;
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int target = stoi(ks);
+        Solution ob;
+        vector<int> ans = ob.sumClosest(arr, target);
+        if (ans.empty()) {
+            cout << "[]";
+        } else {
+            for (auto it : ans) {
+                cout << it << " ";
+            }
+        }
+        cout << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
